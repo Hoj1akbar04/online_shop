f528fc60b3e81cabda909e936fce898f8761a2df
Hojiakbar <hojiakbaribodullayev04@gmail.com>
2024-04-28 19:53:35 +0500

    online shop website

--- core/settings.py
+++ core/settings.py
@@ -131,5 +131,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 #STATICFILES_DIRS = [BASE_DIR, 'static']
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 #MEDIAFILES_DIRS = [BASE_DIR, 'media']
